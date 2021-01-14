9717b54694b7620b3ff92b337139ec7204b9ba9b
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-15 02:19:34 +0300

    models, serializers, views created

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Quiz, Question, Answer
+import nested_admin
 
 # Register your models here.
+
+class AnswerInLine(nested_admin.NestedTabularInline):
+    model = Answer
+    extra = 4
+    max_num = 4
+
+class QuestionInLine(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInLine]
+    max_num = 20
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInLine]
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(Answer)
---
+++ src/quiz/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Category, Quiz
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            "name",
+            "quiz_count"
+        )
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = (
+            "title",
+            "question_count",
+        )
